b6eb262c8a75e9818f0bbefc51e28d6469e33a25
Josué Serejo <josueserejo5@gmail.com>
2026-06-28 10:09:59 -0300

    lock registration form

--- static/js/form.js
+++ static/js/form.js
@@ -1,5 +1,5 @@
 /* --- VII SECOM 2026 - CONTROLE DE LIBERAÇÃO DOS FORMULÁRIOS --- */
-const inscricoesAbertas = true;
+const inscricoesAbertas = false;
 const submissoesAbertas = true;
 
 document.addEventListener("DOMContentLoaded", () => {
